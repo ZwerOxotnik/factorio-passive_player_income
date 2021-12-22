a82f03cc06c8476346941c3c7c25a22312c26ec7
ZwerOxotnik <zweroxotnik@gmail.com>
2021-12-22 22:43:15 +0300

    Update to 0.9.1

--- control.lua
+++ control.lua
@@ -1,1 +1,1 @@
-require("event_handler").add_lib(require("models/passive_player_income"))
+require("__zk-lib__/static-libs/lualibs/event_handler_vZO.lua").add_lib(require("models/passive_player_income"))
